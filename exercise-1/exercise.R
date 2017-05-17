276f2e21b71f63cb56b5192367c91312c2fbc74b
sburd36 <sburd36@uw.edu>
2017-05-17 15:39:03 -0700

    ..

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,14 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("C:/Users/Stephanie/Desktop/In-Class/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source('./scripts/BuildScatter.R')
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+iris <- as.data.frame(iris)
+
+BuildScatter(data = iris, xVar = 'Petal.Width',  yVar = 'Petal.Length', colorVar = 'Species',
+             title = 'Iris Petal Details', xLab = "Petal Width", yLab = "Petal Length")
+
--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -4,12 +4,13 @@
 shinyUI(fluidPage(
   
   # Add a numbericInput to choose the number of observations
-  
+  numericInput("number", label = "observations", value = 50),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput("color", label = "colors", choices = list("Red"= 'red', "Blue" = 'blue')),
   
   # Plot the output with the name "scatter"
-  
+  plotOutput("scatter")
   
 ))
+
